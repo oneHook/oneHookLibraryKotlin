7a344339a120c302361bba62618bbb2cbeb9ea24
Eagle Diao <>
2021-01-11 09:19:50 -0500

    cont.

--- app/src/main/java/com/onehook/onehooklib/ui/controllers/LineGraphDemoViewController.kt
+++ app/src/main/java/com/onehook/onehooklib/ui/controllers/LineGraphDemoViewController.kt
@@ -8,9 +8,12 @@ import com.onehook.onehooklib.ui.reusable.BaseDetailViewController
 import com.onehook.onhooklibrarykotlin.utils.dp
 import com.onehook.onhooklibrarykotlin.view.LP
 import com.onehook.onhooklibrarykotlin.view.MATCH_PARENT
+import com.onehook.onhooklibrarykotlin.widget.LineGraphUIModel
 import com.onehook.onhooklibrarykotlin.widget.LineGraphView
 
 class LineGraphDemoViewController : BaseDetailViewController() {
+
+    lateinit var graph1: LineGraphView
 
     override fun onCreateContentView(inflater: LayoutInflater, container: ViewGroup): View {
         return LinearLayoutCompat(container.context).apply {
@@ -22,7 +25,14 @@ class LineGraphDemoViewController : BaseDetailViewController() {
                     width = MATCH_PARENT
                     height = dp(250)
                 }.linearLayoutLp()
+                graph1 = this
             })
         }
+    }
+
+    override fun viewDidLoad(view: View) {
+        super.viewDidLoad(view)
+
+        graph1.bind(LineGraphUIModel(intArrayOf(1, 5, 4, 2, 4, 5, 2, 3, 1)), animated = false)
     }
 }
--- onehooklibrarykotlin/src/main/java/com/onehook/onhooklibrarykotlin/widget/LineGraphView.kt
+++ onehooklibrarykotlin/src/main/java/com/onehook/onhooklibrarykotlin/widget/LineGraphView.kt
@@ -1,13 +1,29 @@
 package com.onehook.onhooklibrarykotlin.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
 import com.onehook.onhooklibrarykotlin.utils.dpf
+
+
+data class LineGraphUIModel(
+    val points: ArrayList<Float>,
+    val smooth: Boolean = true
+) {
+    constructor(
+        numbers: IntArray,
+        minValue: Int? = null,
+        maxValue: Int? = null,
+        smooth: Boolean = true
+    ) : this(points = arrayListOf<Float>(), smooth = smooth) {
+        val minV = (minValue ?: numbers.minOrNull() ?: 0).toFloat()
+        val maxV = (maxValue ?: numbers.maxOrNull() ?: 0).toFloat()
+        numbers.forEach {
+            points.add(1 - (it.toFloat() - minV) / (maxV - minV))
+        }
+    }
+}
 
 open class LineGraphView @JvmOverloads constructor(
     context: Context,
@@ -16,15 +32,21 @@ open class LineGraphView @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyle, defStyleRes) {
 
+    private var needsRefresh = false
     private val paint = Paint().apply {
         strokeWidth = dpf(2)
         color = Color.WHITE
         style = Paint.Style.STROKE
     }
+    private val gradientPaint = Paint().apply {
+
+    }
     private val path = Path()
+    private var uiModel = LineGraphUIModel(points = arrayListOf(), smooth = false)
 
     init {
         setWillNotDraw(false)
+        setBackgroundColor(Color.RED)
     }
 
     override fun draw(canvas: Canvas?) {
@@ -32,16 +54,76 @@ open class LineGraphView @JvmOverloads constructor(
         if (canvas == null) {
             return
         }
-        canvas.drawColor(Color.RED)
+        if (isInEditMode) {
+            /* For Android studio preview */
+            bind(
+                uiModel = LineGraphUIModel(intArrayOf(2, 5, 1, 4, 2, 3, 1), -2, 6),
+                animated = false
+            )
+        }
+
+
+        canvas.clipPath(path)
+
+        gradientPaint.setShader(
+            LinearGradient(
+                0f, 0f, 0f,
+                canvas.height.toFloat(), Color.WHITE, Color.BLACK, Shader.TileMode.MIRROR
+            )
+        )
+        canvas.drawPaint(gradientPaint)
+
+        paint.style = Paint.Style.STROKE
         canvas.drawPath(path, paint)
+
+
     }
 
     override fun layout(l: Int, t: Int, r: Int, b: Int) {
         super.layout(l, t, r, b)
-        val width = r - l
-        val height = b - t
+        refresh(animated = false)
+    }
+
+    private fun refresh(animated: Boolean) {
+        val width = measuredWidth - paddingLeft - paddingRight
+        val height = measuredHeight - paddingTop - paddingBottom
+        if (!needsRefresh || uiModel.points.isEmpty() || width <= 0 || height <= 0) {
+            return
+        }
+
+        val segmentCount = uiModel.points.size
+        val segmentWidth = width / (segmentCount - 1)
+        val points = mutableListOf<PointF>()
+
+        uiModel.points.forEachIndexed { index, num ->
+            points.add(
+                PointF(
+                    (index * segmentWidth + paddingStart).toFloat(),
+                    height * num + paddingTop
+                )
+            )
+        }
+
         path.reset()
-        path.moveTo(0f, 0f)
-        path.moveTo(dpf(150), dpf(150))
+        points.forEach {
+            if (path.isEmpty) {
+                path.moveTo(it.x, it.y)
+            } else {
+                path.lineTo(it.x, it.y)
+            }
+        }
+        path.lineTo(width.toFloat(), height.toFloat())
+        path.lineTo(paddingLeft.toFloat(), height.toFloat())
+        path.close()
+
+        invalidate()
+        needsRefresh = false
+    }
+
+
+    fun bind(uiModel: LineGraphUIModel, animated: Boolean) {
+        needsRefresh = true
+        this.uiModel = uiModel
+        refresh(animated = animated)
     }
 }
